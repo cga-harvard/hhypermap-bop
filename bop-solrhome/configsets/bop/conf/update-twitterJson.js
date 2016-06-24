65e86035b0763cca650fbb0c635d90f530ec82ab
David Smiley <david.w.smiley@gmail.com>
2016-06-24 00:32:35 -0300

    Added missing license

--- bop-solrhome/configsets/bop/conf/update-twitterJson.js
+++ bop-solrhome/configsets/bop/conf/update-twitterJson.js
@@ -1,6 +1,21 @@
 /*
-  In order for this to be executed, it must be properly wired into solrconfig.xml; by default it is commented out in
-  the example solrconfig.xml and must be uncommented to be enabled.
+ * Copyright 2016 President and Fellows of Harvard College
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+  In order for this to be executed, it must be properly wired into solrconfig.xml.
 */
 
 var Long = Java.type('java.lang.Long');
