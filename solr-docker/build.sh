66fda6fe27ca8131a6b9b7469718402143ace323
David Smiley <david.w.smiley@gmail.com>
2017-01-26 14:51:12 -0500

    solr-docker: use env var for the version (6.3.0-hcga1)

--- solr-docker/build.sh
+++ solr-docker/build.sh
@@ -2,10 +2,12 @@
 
 set -ue
 
+SOLR_VERSION=6.3.0-hcga1
 (cd ../bop/solr-plugins && mvn -DskipTests package)
 
 ln -f ../bop/solr-plugins/target/hcga-bop-solr-plugins.jar .
 
-docker build -t harvardcga/solr .
+docker build -t harvardcga/solr -t harvardcga/solr:$SOLR_VERSION .
 
 docker push harvardcga/solr
+docker push harvardcga/solr:SOLR_VERSION
