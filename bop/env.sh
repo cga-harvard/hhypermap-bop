1f865f96fdd10e0b0b7ea4ec1cd5ef24b883a1a7
David Smiley <david.w.smiley@gmail.com>
2017-02-01 23:10:22 -0500

    bop: tweak solrconfig bop_tweets2: cache tuning. Month shards for 2 years. Output2.

--- bop/env.sh
+++ bop/env.sh
@@ -5,10 +5,13 @@ BOP_ENV="${BOP_ENV:-local}"
 echo "(BOP_ENV=$BOP_ENV)"
 case "$BOP_ENV" in
   "Kontena" ) # Kontena, VPN'ed to MOC
-    ZK_HOST=bop-zookeeper:2181
-    SOLR_URL_COLL=http://bop-solr:8983/solr/bop
-    COLLECTION_OPTS=(-F maxShardsPerNode=4
-      -F createNodeSet=bop-solr-1.kontena.local:8983_solr)
+    #On Docker for Mac, name resolution when VPN to Kontena doesn't reliably resolve
+    ZK_HOST="$(nslookup "bop-zookeeper" | grep -m1 "^Address: " | cut -d' ' -f2):2181"
+    SOLR_URL_COLL=http://bop-solr:8983/solr/bop_tweets2
+    # createNodeSet is sensitive to the SOLR_HOST which is in turn based on
+    #  KONTENA_NODE_NAME which has changed slightly across Kontena versions and seems sticky/node
+    COLLECTION_OPTS=(-F maxShardsPerNode=8
+      -F createNodeSet=bop-solr-1:8983_solr)
 #    COLLECTION_OPTS=(-F maxShardsPerNode=4 \
 #      -F rule=shard:RT,replica:1,role:overseer \
 #      -F rule=shard:!RT,replica:1,role:!overseer~)
