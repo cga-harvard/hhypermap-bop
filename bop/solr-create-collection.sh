35f22ea27244681a94dd675078cf2affa394466f
dkakkar <devikakakkar29@gmail.com>
2017-05-24 13:33:07 -0400

    Delete solr-create-collection.sh

--- bop/solr-create-collection.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-. env.sh
-
-echo "Creating collection $COLLECTION. (Note: will 400 error if already exists)"
-curl -XPOST --fail "$SOLR_URL/admin/collections" -F action=CREATE -F name="$COLLECTION" \
-  -F router.name=implicit -F shards=RT "${COLLECTION_OPTS[@]}"
