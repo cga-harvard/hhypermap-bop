933d1a58d1653d97cd5ea31de6bb26f8859d42db
dkakkar <devikakakkar29@gmail.com>
2017-05-24 13:32:49 -0400

    Delete solr-zk-downconfig.sh

--- bop/solr-zk-downconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-. env.sh
-
-echo "Downloading config $COLLECTION to 'bop' (will overwrite)"
-docker run --rm --net=host -v "$(pwd)/solrhome/configsets/:/hostConfigsSets/" \
-  harvardcga/solr bin/solr zk -downconfig -n "$COLLECTION" -d /hostConfigsSets/bop/ -z "$ZK_HOST"
